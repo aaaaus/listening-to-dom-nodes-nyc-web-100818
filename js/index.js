e7a47fb7d29b0dc8029340cdf3f57f516f9ec6e4
aaaaus <ausluft@gmail.com>
2018-11-21 14:49:03 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+document.addEventListener("DOMContentLoaded", () => {
+
+// select the first p element:
+
+// let p = document.querySelector('p')
+
+// select all p elements:
+
+let p = document.querySelectorAll('p')
+
+//use this function in the addEventListener when p is clicked:
+
+const clickLog = function (e) {
+  console.log(`You clicked ${this.innerHTML} at X ${e.screenX} and Y ${e.screenY}`); //console log message w/ coordinates of click
+  this.style.color = "#00ff00" //turns p text green
+  this.style.backgroundColor = "#444" //turns background color dark
+}
+
+//call addEventListener on p:
+
+// p.addEventListener('click', clickLog)
+
+//call addEventListener click event using clickLog function on all p:
+
+for (i=0; i < p.length; i++) {
+  p[i].addEventListener('click', clickLog)
+}
+
+
+});
